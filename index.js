3ec1793e4fbb38280ccd54e779f2a25f9510f792
Harsh Rathore <harsh@knytra.com>
2026-04-26 16:47:12 +0530

    Set Firebase function region to asia-southeast1

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = next({ dev, conf: { distDir: '.next' } });
 const handle = app.getRequestHandler();
 const appPrepare = app.prepare();
 
+functions.setGlobalOptions({ region: 'asia-southeast1' });
+
 exports.nextServer = functions.https.onRequest((req, res) => {
   return appPrepare.then(() => handle(req, res));
 });
